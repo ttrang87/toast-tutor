10f37cbbfa9f445b9a1e1eea64a555c84851ca0a
Shepe1304 <hqgiang1346@gmail.com>
2025-01-09 16:45:21 -0500

    Authentication backend setup, registration view added, and connected auth routes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,12 @@ from pathlib import Path
 from decouple import config
 import dj_database_url
 
+from datetime import timedelta
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -44,7 +50,23 @@ SECRET_KEY = 'django-insecure-_*wh%jg(pq5n3=$*14px-o+p=bf6nqxtso1s7o%^iipik^nh%0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "*"
+]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
 
 
 # Application definition
@@ -59,6 +81,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'toast_tutor',
+    "api",
 ]
 
 MIDDLEWARE = [
@@ -70,6 +93,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -138,3 +162,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWS_CREDENTIALS = True
