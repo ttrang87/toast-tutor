509f39cc9155dfa82b510306c79e34259a92cddf
Qzanhh <quanganhnguyen1562@gmail.com>
2025-06-05 13:29:17 +0700

    solved unmerged conflict

--- frontend/src/components/landing/Header.jsx
+++ frontend/src/components/landing/Header.jsx
@@ -46,7 +46,6 @@ const Header = (props) => {
             Profile
           </button>
         )}
-<<<<<<< HEAD
         <button
           className="py-2 w-20 rounded-full bg-transparent hover:bg-gray-400/20 transition-all duration-200"
           onClick={() => navigate("/meetings/create")}
@@ -60,12 +59,6 @@ const Header = (props) => {
           Calendar
         </button>
         <button className="py-2 w-20 rounded-full bg-transparent hover:bg-gray-400/20 transition-all duration-200">
-=======
-        <button className="py-2 w-20 rounded-full bg-transparent hover:bg-gray-400/20 transition-all duration-200"
-        onClick={() =>
-          navigate('/listing')
-        }>
->>>>>>> ea5db48b5eb3800cabdf1a73de670212df493312
           Tutors
         </button>
         <button className="py-2 w-20 rounded-full bg-transparent hover:bg-gray-400/20 transition-all duration-200">
