00527e85c498df7a9e960349880b005edccc6702
Qzanhh <quanganhnguyen1562@gmail.com>
2025-06-10 10:39:41 +0700

    check

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -190,14 +190,6 @@ STRIPE_PUBLIC_KEY = os.getenv("PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.getenv("SECRET_KEY")
 STRIPE_WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
 
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'toasttutorreach@gmail.com'
-# EMAIL_HOST_PASSWORD = 'wupofgoejcngvwob'
-
 # FRONTEND_BASE_URL = 'http://localhost:5173'
 
 # Celery settings
